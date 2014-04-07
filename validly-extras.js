6594e33755566279011a0090c7a57cc843ff8d18
rockerest <tom@vu>
2014-04-07 17:02:12 -0500

    Corrected Validly calls

--- validly-extras.js
+++ validly-extras.js
@@ -6,11 +6,11 @@ define(
 
         /****** Add / export functionality ******/
         Validly = Validly.plugin( "isIpv4", function( input ){
-            return validator.matches( /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/, input );
+            return validator.pattern( /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/, input );
         });
 
         Validly = Validly.plugin( "isIpv6", function( input ){
-            return validator.matches( /^((?=.*::)(?!.*::.+::)(::)?([\dA-F]{1,4}:(:|\b)|){5}|([\dA-F]{1,4}:){6})((([\dA-F]{1,4}((?!\3)::|:\b|$))|(?!\2\3)){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$/i, input );
+            return validator.pattern( /^((?=.*::)(?!.*::.+::)(::)?([\dA-F]{1,4}:(:|\b)|){5}|([\dA-F]{1,4}:){6})((([\dA-F]{1,4}((?!\3)::|:\b|$))|(?!\2\3)){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$/i, input );
         });
 
         return Validly;
